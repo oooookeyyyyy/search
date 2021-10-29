50269543be220660d62b7face54f84f145bd7f05
oooookeyyyyy <73366852+oooookeyyyyy@users.noreply.github.com>
2021-10-29 03:31:40 +0330

    Update search.py

--- search.py
+++ search.py
@@ -2,8 +2,8 @@ from os import path, walk
 
 def search(filename, search_path):
     result = []
-    for root, dir , files in walk(search_path):
-        if filename in dir :
+    for root, dirs , files in walk(search_path):
+        if filename in dirs :
             result.append(path.join(root, filename))
         elif filename in files :
             result.append(path.join(root, filename))
